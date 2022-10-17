ea4e347481ab114c3be43485d929b2088ae01d2b
Marcos Vini <marcosviniciosaraujo@tjse.jus.br>
2022-10-17 20:51:32 -0300

    Commit Inicial - Projeto Biblioteca

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Biblioteca.Domain.Models;
+using Biblioteca.Infra.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    public class AccountController
+    {
+        [ApiController]
+        [Route("v1")]
+        public class LoginController : ControllerBase
+        {
+            [HttpPost]
+            [Route("login")]
+            public async Task<ActionResult<dynamic>> AuthenticateAsync([FromBody] Usuario usuario)
+            {
+                var user = UsuarioRepository.Get(usuario.Username, usuario.Password);
+                if (user == null)
+                    return NotFound(new { message = "Usuário ou senha inválidos." });
+
+                //Gera o Token
+                var token = TokenService.GenerateToken(user);
+
+                //oculta senha
+                user.Password = "";
+
+                //Retorna os dados
+                return new
+                {
+                    user = user,
+                    token = token,
+                };
+
+            }
+
+        }
+    }
+}
